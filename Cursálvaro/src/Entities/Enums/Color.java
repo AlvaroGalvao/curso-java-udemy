08d517d0ecf9a047b6846e8766de0a996e7fb069
AlvaroGalvao <168601500+AlvaroGalvao@users.noreply.github.com>
2025-02-11 15:06:55 -0300

    Delete Cursálvaro/src/Entities directory

--- Cursálvaro/src/Entities/Enums/Color.java
+++
@@ -1,9 +0,0 @@
-package Entities.Enums; // Declaração do pacote onde a enumeração está localizada
-
-// Definição de uma enumeração chamada Color
-public enum Color { 
-	
-	BLACK, // Representa a cor preta
-	BLUE,  // Representa a cor azul
-	RED;   // Representa a cor vermelha
-}
