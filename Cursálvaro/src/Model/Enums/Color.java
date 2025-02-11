2a887dd86f5e7bb2c3eab8698805f4c68ccddcc0
AlvaroGalvao <168601500+AlvaroGalvao@users.noreply.github.com>
2025-02-11 15:08:48 -0300

    Add files via upload

---
+++ Cursálvaro/src/Model/Enums/Color.java
@@ -0,0 +1,9 @@
+package Model.Enums; // Declaração do pacote onde a enumeração está localizada
+
+// Definição de uma enumeração chamada Color
+public enum Color { 
+	
+	BLACK, // Representa a cor preta
+	BLUE,  // Representa a cor azul
+	RED;   // Representa a cor vermelha
+}
---
+++ Cursálvaro/src/Model/Enums/WorkerLevel.java
@@ -0,0 +1,21 @@
+// Define o pacote ao qual esta enumeração pertence
+package Model.Enums;
+
+/**
+ * A enumeração `WorkerLevel` representa os diferentes níveis de um trabalhador
+ * em uma organização ou empresa. 
+ * 
+ * Esta enum é útil para categorizar os trabalhadores de acordo com sua experiência
+ * ou posição hierárquica.
+ */
+public enum WorkerLevel {
+    
+    // Representa um trabalhador de nível Júnior (início de carreira)
+    JUNIOR,
+
+    // Representa um trabalhador de nível intermediário
+    MID_LEVEL,
+
+    // Representa um trabalhador de nível Sênior (mais experiente)
+    SENIOR;
+}
